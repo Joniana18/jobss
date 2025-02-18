8e693d542a6fd81fc96e8d65f6ecc67ee3199e13
Joniana18 <129261600+Joniana18@users.noreply.github.com>
2025-02-18 07:34:41 +0100

    Update scraper_selenium.py

--- scraper_selenium.py
+++ scraper_selenium.py
@@ -97,7 +97,7 @@ def scrape_all_jobs_selenium(base_url, site_type, start_page=1):
 
             # Check for the presence of a "Next" button
             if site_type == "duapune":
-                next_button = driver.find_elements(By.LINK_TEXT, "Vijuese »")
+                next_button = driver.find_elements(By.CSS_SELECTOR, "a.relative.inline-flex.items-center.px-4.py-2.ml-3.text-sm.font-medium.text-gray-700.bg-white.border.border-gray-300.leading-5.rounded-md.hover:text-gray-500.focus:outline-none.focus:ring.ring-gray-300.focus:border-blue-300.active:bg-gray-100.active:text-gray-700.transition.ease-in-out.duration-150")
             elif site_type == "punajuaj":
                 next_button = driver.find_elements(By.CSS_SELECTOR, "a.next.page-numbers")
                 
